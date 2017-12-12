4b985d4bd3680295789571a07d70803a9cbf8756
drakefly <csenat15@outlook.com>
2017-12-12 21:10:56 +0100

    wallah

--- src/ProgrammationSemaine.java
+++ src/ProgrammationSemaine.java
@@ -7,7 +7,28 @@ public class ProgrammationSemaine {
     private HashMap<PieceTheatre, FileSeanceTheatre> maptheatre;
 
 
-    public ProgrammationSemaine(int semaine, PriorityQueue<SeanceCinema> seanceCinemas, HashMap<SeanceTheatre, PieceTheatre> seanceDePiece) {
+    public void ProgrammationSemaine(int semaine, PriorityQueue<SeanceCinema> seanceCinemas, HashMap<SeanceTheatre, PieceTheatre> seanceDePiece) {
         Semaine = semaine;
+    }
+
+    public void ajouterFilmSeance(Film film, FileSeanceCine fileSeanceCine){
+        mapcine.put(film, fileSeanceCine);
+    }
+    public String getSeancescine(Film film) {
+        if(!mapcine.containsKey(film)) throw new IllegalArgumentException();
+        FileSeanceCine file= new FileSeanceCine();
+        file=mapcine.get(film);
+        return file.toString();
+    }
+
+    public void ajouterTheatreSeance(PieceTheatre pieceTheatre,FileSeanceTheatre fileSeanceTheatre){
+        maptheatre.put(pieceTheatre,fileSeanceTheatre);
+    }
+
+    public String getSeanceTheatre(PieceTheatre pieceTheatre) {
+        if(!maptheatre.containsKey(pieceTheatre)) throw new IllegalArgumentException();
+        FileSeanceTheatre file = new FileSeanceTheatre();
+        file= maptheatre.get(pieceTheatre);
+        return file.toString();
     }
 }
